ad511bd5641b4b71511614e044fd6d4e0d582a1b
Kevin VALERIO <valerio.kevin83@gmail.com>
2019-06-03 15:01:43 +0200

    Adding permission

--- app/src/main/java/com/corp/vbdd/vbdd_queueandroid/main/models/RESTHandler.java
+++ app/src/main/java/com/corp/vbdd/vbdd_queueandroid/main/models/RESTHandler.java
@@ -1,6 +1,9 @@
 package com.corp.vbdd.vbdd_queueandroid.main.models;
 
 import android.util.Log;
+import android.widget.Toast;
+
+import com.corp.vbdd.vbdd_queueandroid.main.activities.MainActivity;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -10,7 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RESTHandler {
 
-    private static final String __BACK_URL__ = "https://10.0.2.2:9428";
+    private static final String __BACK_URL__ = "http://" + Config.__IP_ADDRESS__ + ":9428";
     private QueueService queueService;
 
     public RESTHandler() {
@@ -24,7 +27,6 @@ public class RESTHandler {
     public void nextPerson(Integer queueId) {
         Call<Visitor> call = queueService.nextPerson(queueId);
         call.enqueue(new Callback<Visitor>() {
-
             @Override
             public void onResponse(Call<Visitor> call, Response<Visitor> response) {
                 Log.d("xx", "onResponse: " + response.body());
@@ -32,8 +34,7 @@ public class RESTHandler {
 
             @Override
             public void onFailure(Call<Visitor> call, Throwable t) {
-                Log.d("xx", "onFailure: " + t.getMessage());
-
+//                 Toast.makeText(MainActivity.class, "xx", Toast.LENGTH_LONG);
             }
         });
      }
