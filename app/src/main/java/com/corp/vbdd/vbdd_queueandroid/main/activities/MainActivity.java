0ac6f6fbd4f638c9844a3a3e89c47490cafbb479
BauceCamille <camille.bauce@etu.univ-cotedazur.fr>
2019-06-04 10:02:29 +0200

    added log in and log out layout and layout logic #9

--- app/src/main/java/com/corp/vbdd/vbdd_queueandroid/main/activities/MainActivity.java
+++ app/src/main/java/com/corp/vbdd/vbdd_queueandroid/main/activities/MainActivity.java
@@ -2,8 +2,13 @@ package com.corp.vbdd.vbdd_queueandroid.main.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.corp.vbdd.vbdd_queueandroid.R;
 import com.corp.vbdd.vbdd_queueandroid.main.models.RESTHandler;
 
@@ -11,20 +16,91 @@ public class MainActivity extends AppCompatActivity {
 
     RESTHandler restHandler;
 
+    Button nextButton;
+    Button prevButton;
+    Button connexionButton;
+    Button nextPersonBecauseAFK;
+    Button logOutBtn;
+
+    TextView mainInformationText;
+    TextView queueConnectedText;
+    EditText queueIdEditText;
+
+    LinearLayout connectedLayout;
+
+    int queueId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button nextButton = findViewById(R.id.nextBtn);
-        Button prevButton = findViewById(R.id.previousBtn);
-        EditText queueId = findViewById(R.id.queueId);
+        nextButton = findViewById(R.id.nextBtn);
+        prevButton = findViewById(R.id.previousBtn);
+        connexionButton = findViewById(R.id.connexionBtn);
+        nextPersonBecauseAFK = findViewById(R.id.nextPersonBecauseAFK);
+        logOutBtn = findViewById(R.id.logOutBtn);
+
+        queueIdEditText = findViewById(R.id.queueId);
+        mainInformationText = findViewById(R.id.mainInformation);
+        queueConnectedText = findViewById(R.id.queueConnectedText);
+        connectedLayout = findViewById(R.id.connectedLayout);
 
         restHandler = new RESTHandler(MainActivity.this);
 
-        nextButton.setOnClickListener(click -> this.restHandler.nextPerson(Integer.valueOf(queueId.getText().toString())));
-        prevButton.setOnClickListener(click -> this.restHandler.previousPerson(Integer.valueOf(queueId.getText().toString())));
+        nextButton.setOnClickListener(click -> this.restHandler.nextPerson(queueId));
+        prevButton.setOnClickListener(click -> this.restHandler.previousPerson(queueId));
+        connexionButton.setOnClickListener(click -> logIn());
+        logOutBtn.setOnClickListener(click -> logOut());
+
+        initialize();
+    }
+
+    private void initialize(){
+        connexionButton.setVisibility(View.VISIBLE);
+        mainInformationText.setVisibility(View.INVISIBLE);
+        connectedLayout.setVisibility(View.INVISIBLE);
+
+        nextButton.setEnabled(false);
+        prevButton.setEnabled(false);
+        nextPersonBecauseAFK.setEnabled(false);
+    }
+
+    private void logIn(){
+
+        // TODO: Verify if the queue exist in back-end
+
+        queueId = Integer.valueOf(queueIdEditText.getText().toString());
+        connectedLayout.setVisibility(View.VISIBLE);
+        mainInformationText.setVisibility(View.VISIBLE);
+        connexionButton.setVisibility(View.INVISIBLE);
+        queueIdEditText.setVisibility(View.INVISIBLE);
+
+        queueConnectedText.setText(getResources().getString(R.string.queue_connected, queueId));
+
+        nextButton.setEnabled(true);
+        prevButton.setEnabled(true);
+        nextPersonBecauseAFK.setEnabled(true);
+
+        // hide keyboard
+        try {
+            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        } catch (Exception e) {
+
+        }
+}
+
+    private void logOut() {
+        connectedLayout.setVisibility(View.INVISIBLE);
+        mainInformationText.setVisibility(View.INVISIBLE);
+        connexionButton.setVisibility(View.VISIBLE);
+        queueIdEditText.setVisibility(View.VISIBLE);
+
+        nextButton.setEnabled(false);
+        prevButton.setEnabled(false);
+        nextPersonBecauseAFK.setEnabled(false);
     }
 
 }
